f5cc3d111e48b586c4e0ef4c855f4f6735ebfe32
Vic <125237471+vicsanity623@users.noreply.github.com>
2025-12-30 01:52:01 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const GAME_VERSION = 'v0.1.6'; // Incremented to force update
+const GAME_VERSION = 'v0.1.7'; // Incremented to force update
 const CACHE_NAME = `art-${GAME_VERSION}`;
 
 // Assets to cache immediately on install
